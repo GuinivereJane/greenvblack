b00a07755a3868862f8436105ac5152ba51adb12
Guin Kellam <guin@sport-travel.com>
2019-03-01 16:02:35 -0500

    minaml styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,164 @@
 <template>
-  <v-container  grid-list-md text-xs-center>
+  <v-container class="text"  grid-list-md text-xs-center>
     <v-layout row wrap>
-      <v-flex  xs6>
-        <v-card dark color="green" height="50rem">
-          <v-card-text class="px-0">
-            GREEN SCORE {{ greenScoreByScore }}</br>
-            GREEN POINTS {{ greenScoreByPoints }}
-          </v-card-text>
-        </v-card>
+      <v-flex class="size" xs6>
+        <div class="green">
+          GREEN SCORE {{ greenScoreByScore }}</br>
+          GREEN POINTS {{ greenScoreByPoints }}
+        </div>
       </v-flex>
-      <v-flex  xs6>
-       <v-card dark height="50rem">
-          <v-card-text color="green" class="px-0">
+      <v-flex class="size"  xs6>
+        <div class="black">
            BLACK SCORE {{ blackScoreByScore }}<br>
            BLACK POINTS {{ blackScoreByPoints }}
-          </v-card-text>
-        </v-card>
+        </div>
       </v-flex>
+      <v-flex xs2></v-flex>
+      <v-flex align-content-center xs8>
+        <div>
+        <img src="~assets/images/download.jpeg" alt="Smiley face">
+        </div>
+      </v-flex>
+      <v-flex xs2></v-flex>
     </v-layout>
   </v-container>
 </template>
-
+<style>
+img {
+  width: 100%;
+}
+.size {
+  height:50rem;
+}
+.text {
+  color:white;
+}
+div{
+  height: 100%;
+}
+  .green {
+    background-color: green;
+  }
+   .black {
+    background-color: black;
+  }
+</style>
 <script>
 const _ = require('lodash');
-//balck
-//Green Dave, Emma
+
 
 const teamBlack = [1668782, 1654875, 1518645, 1626216, 1668672, 708073, 946872, 708022, 1575134, 1700999, 952877, 1253849, 1275746, 371298, 347291, 1693243]
-const teamGreen = [ 417420, 973378, 397804, 708103, 773883, 706490, 1262251, 1304578, 1276204, 1632490, 1550233, 687298, 1654168, 1355793, 1677273 ];
+const teamGreen = [ 1202565, 1397696,417420, 973378, 397804, 708103, 773883, 706490, 1262251, 1304578, 1276204, 1632490, 1550233, 687298, 1654168, 1355793, 1677273 ];
 export default {
   async asyncData({ $axios }) {
     const mens = await $axios.$get('https://games.crossfit.com/competitions/api/v1/competitions/open/2019/leaderboards?affiliate=13624&division=1&scaled=0&page=1')
     const mensscaled = await $axios.$get('https://games.crossfit.com/competitions/api/v1/competitions/open/2019/leaderboards?affiliate=13624&division=1&scaled=1&page=1')
     const womens = await $axios.$get('https://games.crossfit.com/competitions/api/v1/competitions/open/2019/leaderboards?affiliate=13624&division=2&scaled=0&page=1')
     const womensscaled = await $axios.$get('https://games.crossfit.com/competitions/api/v1/competitions/open/2019/leaderboards?affiliate=13624&division=2&scaled=1&page=1')
-    return { mens, mensscaled, womens, womensscaled }
+    //to get dave
+    const mensover5559 = await $axios.$get('https://games.crossfit.com/competitions/api/v1/competitions/open/2019/leaderboards?affiliate=13624&division=7&scaled=0&page=1');
+    //to get Emma
+    const womensunder1415 = await $axios.$get('https://games.crossfit.com/competitions/api/v1/competitions/open/2019/leaderboards?affiliate=13624&division=15&scaled=0&page=1');
+    return { mens, mensscaled, womens, womensscaled, mensover5559, womensunder1415 }
   },
+
   computed: {
       atheletes () {
+        console.log(teamBlack.length + teamGreen.length);
         let mens = this.divisionMap(this.mens.leaderboardRows);
         let womens = this.divisionMap(this.womens.leaderboardRows);
-        return womens.concat(mens);
+        let mensover5559 = this.divisionMap(this.mensover5559.leaderboardRows);
+        let womensunder1415 = this.divisionMap(this.womensunder1415.leaderboardRows);
+        return this.addPoints(womens.concat(mens).concat(mensover5559).concat(womensunder1415));
       },
       blackScoreByScore(){
-        let test = teamBlack.reduce((accum, val, key)=>{
-          accum += parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).scores[0].score,10);
+        return teamBlack.reduce((accum, val, key)=>{
+          accum += parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).totalScore,10);
           return accum
         }, 0);
-        return test;
+        //return test;
       },
       blackScoreByPoints () {
-        console.log(teamBlack.length);
-        console.log('----------------');
         let test = teamBlack.reduce((accum, val, key)=>{
-          console.log(parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).overallScore,10));
-          accum += parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).overallScore,10);
+          console.log(accum+"+"+ parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).points,10));
+          accum += parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).points,10);
           return accum
         }, 0);
         return test;
       },
       greenScoreByScore(){
         let test = teamGreen.reduce((accum, val, key)=>{
-          accum += parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).scores[0].score,10);
+          accum += parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).totalScore,10);
           return accum
         }, 0);
         return test;
       },
       greenScoreByPoints(){
         let test = teamGreen.reduce((accum, val, key)=>{
-          accum += parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).overallScore,10);
+          accum += parseInt(_.find(this.atheletes, athelete => athelete.competitorId == val).points,10);
           return accum
         }, 0);
         return test;
       },
-
-
     },
 
     methods: {
       divisionMap(data) {
         if (data) {
         return data.map(athlete => {
-          return {...athlete.entrant, scores: [...athlete.scores], overallRank: athlete.overallRank ,overallScore: athlete.overallScore}
+          return {...athlete.entrant, scores: [...athlete.scores], overallRank: athlete.overallRank }
         });
         } else {
           return []
         }
       },
+      addTotalScore(atheletes){
+       return atheletes.map(athelete =>{
+          let score = athelete.scores.reduce((accum, score, key) => {
+            accum += parseInt(score.score)
+            return accum;
+          }, 0);
+          return { ...athelete, totalScore:score };
+        });
+      },
+      sortAtheltesByScore(atheletes){
+        return atheletes.sort((a,b) => {
+          if ( a.totalScore < b.totalScore){
+            return 1;
+          }
+          if ( a.totalScore > b.totalScore){
+            return -1;
+          }
+          return 0;
+        });
+      },
+      calculatePoints(totals){
+        var points = 2;
+        let numTie = 0;
+        for(let x=0; x < totals.length; x++){
+          if(totals[x] && totals[x+1]){
+            if (totals[x].totalScore === totals[x+1].totalScore){
+              numTie++;
+              totals[x].points = points;
+            } else {
+              totals[x].points = points;
+              points = points+numTie;
+              numTie = 0;
+              points++;
+            }
+          } else {
+            totals[x].points = points;
+          }
+        }
+        return totals;
+      },
+      addPoints(atheletes){
+        let totals = this.addTotalScore(atheletes);
+        totals = this.sortAtheltesByScore(totals);
+        totals = this.calculatePoints(totals);
 
-      getScoreFromLeaderboardData (leaderboardRows) {
-
-      }
+        return totals;
+      },
     }
 }
 </script>
